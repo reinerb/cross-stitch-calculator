46d43cb013a8e7237478e493121915a119532d30
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-06 14:45:02 -0400

    Fix computation area

--- src/utils/functions/deriveStats.ts
+++ src/utils/functions/deriveStats.ts
@@ -1,4 +1,4 @@
-import { DerivedStitchStats, StitchStats } from '../types/types';
+import { DerivedStitchStats, StitchStats } from "../types/types";
 
 function roundToEighth(n: number): number {
   return Number((Math.round(n * 8) / 8).toFixed(3));
@@ -14,8 +14,8 @@ export function deriveStats({
 }: StitchStats): DerivedStitchStats {
   const stitchedWidth = roundToEighth(width / (threadCount / threadCoverage));
   const stitchedHeight = roundToEighth(height / (threadCount / threadCoverage));
-  const totalWidth = stitchedWidth + borderSize + finishingSize;
-  const totalHeight = stitchedHeight + borderSize + finishingSize;
+  const totalWidth = stitchedWidth + borderSize * 2 + finishingSize * 2;
+  const totalHeight = stitchedHeight + borderSize * 2 + finishingSize * 2;
 
   return {
     stitchedWidth,
