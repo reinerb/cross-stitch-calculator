a7c818f0d109cf8e0b097da8b756a5c364b0f549
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-06 16:26:16 -0400

    Adjust DimensionDisplay

--- src/utils/components/DimensionsDisplay.tsx
+++ src/utils/components/DimensionsDisplay.tsx
@@ -13,17 +13,13 @@ function DimensionsDisplay({ title, width, height, className }: Props) {
       <h2 className="text-center text-lg font-semibold">{title}</h2>
       <div className="flex items-center gap-4">
         <p className="text-center">
-          <span className="block text-center text-xl font-bold">
-            {width} in
-          </span>
-          <span className="block text-sm">wide</span>
+          <span className="block text-center text-xl font-bold">{width}</span>
+          <span className="block text-sm">inches wide</span>
         </p>
         <p className="text-2xl">&times;</p>
         <p className="text-center">
-          <span className="block text-center text-xl font-bold">
-            {height} in
-          </span>
-          <span className="block text-sm">high</span>
+          <span className="block text-center text-xl font-bold">{height}</span>
+          <span className="block text-sm">inches high</span>
         </p>
       </div>
     </section>
