acde316104a7486886f806d896505e6c76df1bf9
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-06 12:58:57 -0400

    Finish adding dark mode styles

--- src/App.tsx
+++ src/App.tsx
@@ -27,21 +27,24 @@ export default function CrossStitchCalculator() {
   };
 
   return (
-    <div
-      data-mode={isDarkMode ? "dark" : "light"}
-      className="grid min-h-screen place-items-center bg-primary-300 dark:bg-primary-900 sm:bg-primary-100 dark:sm:bg-primary-700"
-    >
-      <main className="grid-rows-primary grid max-w-md items-center gap-4 rounded-lg bg-primary-300 p-6 dark:bg-primary-900 sm:shadow-lg">
-        <h1 className="text-center text-lg font-bold sm:text-xl">
-          Cross Stitch Fabric Size Calculator
-        </h1>
-        <StatsForm
-          stats={stats}
-          onChange={handleChange}
-          className="row-span-2"
-        />
-        <DerivedStatsDisplay stats={stats} />
-      </main>
+    <div className={isDarkMode ? "dark" : "light"}>
+      <div
+        className={
+          "grid min-h-screen place-items-center bg-primary-300 dark:bg-primary-900 sm:bg-primary-100 dark:sm:bg-primary-700"
+        }
+      >
+        <main className="grid-rows-primary grid max-w-md items-center gap-4 rounded-lg bg-primary-300 p-6 text-slate-950 dark:bg-primary-900 dark:text-slate-50 sm:shadow-lg">
+          <h1 className="text-center text-lg font-bold sm:text-xl">
+            Cross Stitch Fabric Size Calculator
+          </h1>
+          <StatsForm
+            stats={stats}
+            onChange={handleChange}
+            className="row-span-2"
+          />
+          <DerivedStatsDisplay stats={stats} />
+        </main>
+      </div>
     </div>
   );
 }
